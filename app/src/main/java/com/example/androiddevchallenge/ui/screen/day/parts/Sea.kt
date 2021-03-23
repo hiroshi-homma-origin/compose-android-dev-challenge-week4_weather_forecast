b0bb1ef9452ec3dd9d7729267de7ed14c773770f
hiroshi-homma-origin <hiros.200063@gmail.com>
2021-03-23 12:14:21 +0900

    base imp create (day & night)

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/day/parts/Sea.kt
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.screen.day.parts
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
+
+@SuppressLint("ComposableNaming")
+@Composable
+fun sea() {
+    Image(
+        modifier = Modifier.fillMaxWidth()
+            .offset(y = 433.dp),
+        painter = painterResource(R.drawable.sea),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(70.dp)
+            .height(7.dp)
+            .offset(x = 190.dp, y = 490.dp),
+        painter = painterResource(R.drawable.flow1),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(60.dp)
+            .height(4.dp)
+            .offset(x = 50.dp, y = 510.dp),
+        painter = painterResource(R.drawable.flow2),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(160.dp)
+            .height(5.dp)
+            .offset(x = 120.dp, y = 520.dp),
+        painter = painterResource(R.drawable.flow3),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(100.dp)
+            .height(5.dp)
+            .offset(x = 170.dp, y = 470.dp),
+        painter = painterResource(R.drawable.flow4),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(100.dp)
+            .height(5.dp)
+            .offset(x = 290.dp, y = 460.dp),
+        painter = painterResource(R.drawable.flow5),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(100.dp)
+            .height(5.dp)
+            .offset(x = 280.dp, y = 440.dp),
+        painter = painterResource(R.drawable.flow6),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.width(100.dp)
+            .height(7.dp)
+            .offset(x = 70.dp, y = 478.dp),
+        painter = painterResource(R.drawable.flow7),
+        contentDescription = null
+    )
+    Image(
+        modifier = Modifier.wrapContentWidth()
+            .height(7.dp)
+            .offset(x = 10.dp, y = 450.dp),
+        painter = painterResource(R.drawable.flow8),
+        contentDescription = null
+    )
+}
+
+@SuppressLint("ComposableNaming")
+@Preview
+@Composable
+fun seaPreview() {
+    Box(Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxWidth()
+                .offset(y = 434.dp),
+            painter = painterResource(R.drawable.sea),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(70.dp)
+                .height(7.dp)
+                .offset(x = 190.dp, y = 490.dp),
+            painter = painterResource(R.drawable.flow1),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(60.dp)
+                .height(4.dp)
+                .offset(x = 50.dp, y = 510.dp),
+            painter = painterResource(R.drawable.flow2),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(160.dp)
+                .height(5.dp)
+                .offset(x = 120.dp, y = 520.dp),
+            painter = painterResource(R.drawable.flow3),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(100.dp)
+                .height(5.dp)
+                .offset(x = 170.dp, y = 470.dp),
+            painter = painterResource(R.drawable.flow4),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(100.dp)
+                .height(5.dp)
+                .offset(x = 290.dp, y = 460.dp),
+            painter = painterResource(R.drawable.flow5),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(100.dp)
+                .height(5.dp)
+                .offset(x = 280.dp, y = 440.dp),
+            painter = painterResource(R.drawable.flow6),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.width(100.dp)
+                .height(7.dp)
+                .offset(x = 70.dp, y = 478.dp),
+            painter = painterResource(R.drawable.flow7),
+            contentDescription = null
+        )
+        Image(
+            modifier = Modifier.wrapContentWidth()
+                .height(7.dp)
+                .offset(x = 10.dp, y = 450.dp),
+            painter = painterResource(R.drawable.flow8),
+            contentDescription = null
+        )
+    }
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/night/parts_night/Moon.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.screen.night.parts_night
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
+
+@SuppressLint("ComposableNaming")
+@Composable
+fun moon() {
+    Image(
+        modifier = Modifier.size(120.dp)
+            .offset(x = 250.dp, y = 140.dp),
+        painter = painterResource(R.drawable.crescent_moon),
+        contentDescription = null
+    )
+}
+
+@SuppressLint("ComposableNaming")
+@Preview
+@Composable
+fun moonPreview() {
+    Box(Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.size(120.dp)
+                .offset(x = 250.dp, y = 140.dp),
+            painter = painterResource(R.drawable.crescent_moon),
+            contentDescription = null
+        )
+    }
+}
